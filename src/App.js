e3df2f650a7afdfe0f7b0f93d20df2bef920a5ea
simrankhinvasara <simrankhinvasara@gmail.com>
2021-03-28 12:54:39 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React , {Component} from 'react' ;
 import './App.css';
 
-function App() {
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters : [],
+      searchField : "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then( user => this.setState({ monsters : user }))
+  };
+
+  handleChange = e => {
+    this.setState({searchField : e.target.value})
+  };
+
+render() {
+  const { monsters, searchField } = this.state;
+	const filteredMonsters = monsters.filter(monster =>
+		monster.name.toLowerCase().includes(searchField.toLowerCase())
+	);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox 
+      placeholder = "Search Monsters" 
+      handleChange = {this.handleChange}
+      />
+      <CardList monsters={filteredMonsters} />
+      <h1>{filteredMonsters.length === 0 ? "No results" : ""}</h1>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
